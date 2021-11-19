b734c80cabc3b00e8e56a73d9d0a70802089e242
viniflorencio <viniflorenciomid@gmail.com>
2021-11-19 10:18:06 -0300

    feat pagina criar perfil

---
+++ front-xbox/src/pages/CriarPerfil/CriarPerfil.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './CriarPerfil.css';
+
+export default function CriarPerfil() {
+    return (
+        <div className="criar-container">
+            <h2> Criar Perfil </h2>
+            
+        </div>
+    )
+}
