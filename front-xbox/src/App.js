7a624f3ecfa211fd6470677ae5566ee6b456ec03
viniflorencio <viniflorenciomid@gmail.com>
2021-11-19 10:17:03 -0300

    feat rotas para as paginas

--- front-xbox/src/App.js
+++ front-xbox/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router,Switch, Route } from 'react-router-dom';
+import Home from './pages/Home/Home'
+import Perfil from './pages/Perfil/Perfil';
+import Admin from './pages/Admin/Admin';
+import AddJogo from './pages/AddJogo/AddJogo';
+import CriarPerfil from './pages/CriarPerfil/CriarPerfil';
+import Login from './pages/Login/Login';
+import EditarPerfil from './pages/EditarPerfil/EditarPerfil';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Switch>
+        <Route path='/usuario'/>
+        <Route path='/' exact={true} component={Home}/>
+        <Route path='/perfil' exact={true} component={Perfil}/>
+        <Route path='/admin' exact={true} component={Admin}/>
+        <Route path='/AddJogo' exact={true} component={AddJogo}/>
+        <Route path='/CriarPerfil' exact={true} component={CriarPerfil}/>
+        <Route path='/Login' exact={true} component={Login}/>
+        <Route path='/EditarPerfil' exact={true} component={EditarPerfil}/>
+        <Route path='/CriarPerfil' exact={true} component={CriarPerfil}/>
+
+        </Switch>
+    </Router>
     </div>
-  );
+  )
 }
 
 export default App;
